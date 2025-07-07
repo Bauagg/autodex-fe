ee051f313c438cbf5c6a1d5a12397494516ce3a2
A. Mambaus Sholihin <bauagg87@gmail.com>
2025-07-07 11:59:43 +0700

    Fix: Project Detail

--- src/pages/MenuBar/index.jsx
+++ src/pages/MenuBar/index.jsx
@@ -5,15 +5,12 @@ import Cookies from 'js-cookie'
 import { DashboardIcon, LeftIcon, RightIcon, LogoutIcon } from "../../GlobalComponent/icon"
 import DashboardPage from '../Dashboard'
 import axios from 'axios'
-import { Outlet } from 'react-router-dom'
 
 const MenuBar = () => {
     const [activeMenu, setActiceMenu] = useState("Dashboard")
     const [miniNav, setMiniNav] = useState(false)
     const [dataProfile, setDataProfile] = useState({})
     const navigate = useNavigate();
-    const { pathname } = useLocation();
-    const params = useParams
     useEffect(() => {
         axios.get(`${process.env.REACT_APP_API_URL}/api/profil`, { headers: { Authorization: `Bearer ${Cookies.get('token')}` } })
             .then((result) => {
