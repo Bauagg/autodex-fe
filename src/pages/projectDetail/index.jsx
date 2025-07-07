3a436b89e71d5af3d7d8b2b793c16b1d7d6f6290
A. Mambaus Sholihin <bauagg87@gmail.com>
2025-07-07 11:34:24 +0700

    belajar CI CD dasar

--- src/pages/projectDetail/index.jsx
+++ src/pages/projectDetail/index.jsx
@@ -1,36 +1,36 @@
 import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie'
 import axios from 'axios'
 import {
     IconBell,
-    IconTotalProject,
-    CreateProjectIcon,
     TrashIcon,
     UploadFileIcon,
+    BlueprintIcon,
+    EyeIcon,
+    DashboardIcon,
+    LeftIcon,
+    RightIcon,
+    LogoutIcon,
 } from '../../GlobalComponent/icon';
-import { BlueprintIcon, EyeIcon, } from '../../GlobalComponent/icon';
-import { useNavigate } from 'react-router-dom';
-import { DashboardIcon, LeftIcon, RightIcon, LogoutIcon } from "../../GlobalComponent/icon"
 
 
 export default function ProjectDetail() {
     const location = useLocation();
     const navigate = useNavigate();
-    const [activeMenu, setActiceMenu] = useState("")
     const [miniNav, setMiniNav] = useState(false)
     const [models, setModels] = useState([]);
     const [dataProfile, setDataProfile] = useState({})
     const [selectedFile, setSelectedFile] = useState(null);
     const [data, setData] = useState('');
 
-    // const receivedData = location.state;
     const token = Cookies.get('token');
 
     const handleLogout = () => {
         Cookies.remove('token');
         navigate('/');
     }
+
     const handleRemoveFile = (name) => {
         axios.delete(`${process.env.REACT_APP_API_URL}/api/models/${name}`, { headers: { Authorization: `Bearer ${token}` } })
             .then((res) => {
@@ -39,8 +39,8 @@ export default function ProjectDetail() {
                 console.log(err);
             })
     }
+
     const uploadFile = (file) => {
-        console.log('fungsi dijalankan');
         const receivedData = location.state;
         const body = {
             modelFile: file,
@@ -61,9 +61,8 @@ export default function ProjectDetail() {
                 console.error("Error uploading file:", err);
             });
     };
-    const handleFileChange = (event) => {
-        console.log('step 1 jalan');
 
+    const handleFileChange = (event) => {
         const file = event.target.files[0];
         if (file && file.name.endsWith(".nwd")) {
             setSelectedFile(file);
@@ -72,6 +71,7 @@ export default function ProjectDetail() {
             alert("Please select a file with a .nwd extension");
         }
     };
+
     const renderProfileName = () => {
         const nameParts = dataProfile.name ? dataProfile.name.split(' ') : [];
         if (nameParts.length > 1) {
@@ -80,29 +80,28 @@ export default function ProjectDetail() {
         }
         return dataProfile.name;
     }
+
     const getModels = () => {
         const receivedData = location.state;
-        axios.get(`${process.env.REACT_APP_API_URL}/api/models/${receivedData._id}`, { headers: { Authorization: `Bearer ${token}` } })
-            .then((res) => {
-                console.log(res);
-
-                setModels(res.data.datas);
-            }).
-            catch((err) => {
-                alert('Could not list models. See the console for more details.');
-                console.error(err);
-            })
+        axios.get(`${process.env.REACT_APP_API_URL}/api/models/${receivedData._id}`, {
+            headers: { Authorization: `Bearer ${token}` }
+        }).then((res) => {
+            setModels(res.data.datas);
+        }).catch((err) => {
+            alert('Could not list models. See the console for more details.');
+            console.error(err);
+        });
     }
+
     useEffect(() => {
-        axios.get(`${process.env.REACT_APP_API_URL}/api/profil`, { headers: { Authorization: `Bearer ${Cookies.get('token')}` } })
-            .then((result) => {
-                console.log(result.data)
-                setDataProfile(result.data.data)
-            })
-            .catch((err) =>{
-                console.log(err);
-            })
-    }, [])
+        axios.get(`${process.env.REACT_APP_API_URL}/api/profil`, {
+            headers: { Authorization: `Bearer ${token}` }
+        }).then((result) => {
+            setDataProfile(result.data.data)
+        }).catch((err) => {
+            console.log(err);
+        })
+    }, [token])
 
     useEffect(() => {
         const receivedData = location.state;
@@ -110,7 +109,7 @@ export default function ProjectDetail() {
             getModels();
             setData(receivedData)
         }
-    }, [selectedFile]);
+    }, [selectedFile, getModels, location.state]);
     return (
         <div className="flex bg-[#171821] w-full">
             <div className={`flex bg-[#fff] w-full`}>
@@ -157,9 +156,9 @@ export default function ProjectDetail() {
                         <section className="flex justify-between items-center mb-10">
                             {data ? (
                                 <p className="font-semibold text-2xl text-[#171821] capitalize">
-                                {data.nama_folder}
-                            </p>
-                            ): null}
+                                    {data.nama_folder}
+                                </p>
+                            ) : null}
                             <div>
                                 <IconBell />
                             </div>
@@ -197,7 +196,7 @@ export default function ProjectDetail() {
                                                 <p className='font-semibold text-[12px] text-[#171821] capitalize'>1.5 GB</p>
                                             </div>
                                         </div>
-                                        <div className='flex items-center gap-[12px] items-center'>
+                                        <div className='flex gap-[12px] items-center'>
                                             <div
                                                 onClick={() => navigate(`/view/${index + 1}`, { state: item.urn })}
                                                 className='cursor-pointer flex gap-[8px] items-center hover:bg-[#fff] duration-300 px-[12px] py-[4px] rounded-[4px]'>
@@ -214,56 +213,6 @@ export default function ProjectDetail() {
                                 )
                             })}
                         </section>
-                        {/* <section className="flex space-x-5 w-full ">
-                            <div className="w-[263px] h-[107px] bg-[#FFFF] rounded-[18px] py-[30px] px-[15px] flex border border-[#EBEBEB] mb-5">
-                                <div className="mr-[10px]">
-                                    <IconTotalProject />
-                                </div>
-                                <div className={`flex flex-col`}>
-                                    <p className="font-semibold text-sm text-[#171821] capitalize">
-                                        Total Project
-                                    </p>
-                                    <p className="font-semibold text-2xl text-[#171821] capitalize">
-                                        {`totalProject`}
-                                    </p>
-                                </div>
-                            </div>
-                        </section> */}
-                        {/* ---- */}
-                        {/* <section className="mb-10">
-                            <div onClick={openModal} className="py-[19px] px-9 h-[102px] w-full border border-[#EBEBEB] flex justify-start items-center rounded-xl cursor-pointer">
-                                <div className="w-[177px] h-[44px] rounded-xl bg-[#171821] border-[.7px] border-[#EAEBED] flex items-center">
-                                    <div className="w-[34px] h-[34px] rounded-full flex items-center ml-5 mr-[10px]">
-                                        <CreateProjectIcon />
-                                    </div>
-                                    <p className="font-sans font-semibold text-sm text-[#FFFFFF] capitalize">
-                                        Create Project
-                                    </p>
-                                </div>
-                            </div>
-                        </section> */}
-                        {/* ----- */}
-                        {/* <section className="grid grid-cols-3 gap-4">
-                            {dataProject.map((i) => {
-                                return (
-                                    <div
-                                        onClick={() => navigate(`/menu/${i.nama_folder}`, { state: i })}
-                                        key={i}
-                                        className="w-full bg-white rounded-2xl px-5 py-8 border border-gray-300 flex justify-between items-center"
-                                    >
-                                        <div className="w-full h-[44px] rounded-xl bg-white flex items-center">
-                                            <div className="w-[34px] h-[34px] rounded-full bg-pink-300 flex items-center ml-5 mr-3">
-                                                <IconTotalProject />
-                                            </div>
-                                            <p className="font-sans font-semibold text-2xl text-gray-800 capitalize">
-                                                {i.nama_folder}
-                                            </p>
-                                        </div>
-                                        <span onClick={() => deleteProject(i._id)}><TrashIcon /></span>
-                                    </div>
-                                );
-                            })}
-                        </section> */}
                     </main>
                 </div>
             </div>
