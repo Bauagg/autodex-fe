cb593ab96bca4eff03d349f828ce54ec4d0a9d93
A. Mambaus Sholihin <bauagg87@gmail.com>
2025-07-07 12:09:20 +0700

    Fix: Project Detail

--- src/pages/projectDetail/index.jsx
+++ src/pages/projectDetail/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback} from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
 import axios from 'axios';
